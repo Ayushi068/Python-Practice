d60951484957592855077ce81096447e777eaf7a
ayushiv23 <ayushi_v@epam.com>
2026-06-01 21:26:39 +0530

    Python Basics

---
+++ Python Basics/hello.py
@@ -0,0 +1,6 @@
+#This is the basic syntax of a Python program. It is a simple program that prints "Hello World!" to the console.
+print("Hello World!")
+
+#Indentation is important in Python. It is used to define the scope of loops, functions, and other code blocks. In this example, we are using indentation to define the scope of the print statement.
+if 5>2:
+    print("Five is greater than two!") 
--- hello.py
+++
@@ -1,2 +0,0 @@
-print("Hello World!")
-print("is git set up done")
