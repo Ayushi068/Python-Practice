6f7858c7649e973eaac2a8198e559a1df9371a76
ayushiv23 <ayushi_v@epam.com>
2026-06-02 10:59:17 +0530

    datatypes

--- Python Basics/L3_datatypes.py
+++ Python Basics/L3_datatypes.py
@@ -10,5 +10,37 @@
 #Numeric Types - int, float, complex
 x = 5 #int
 y = 3.14 #float
+z = 1 + 2j #complex
+
+print(type(x)) #Output: <class 'int'>
+print(type(y)) #Output: <class 'float'>
+print(type(z)) #Output: <class 'complex'>
+
+ 
+x = "Hello World!" #str
+print(type(x)) #Output: <class 'str'>
+
+x = [1,2,3,4,5]  #list
+print(type(x)) #Output: <class 'list'>
+
+x =(1,2,3,4,5) #tuple , immutable
+print(type(x))
+
+x = range(1,10) #range
+print(type(x)) #Output: <class 'range'>
+for i in x:
+    print(i) #Output: 1 2 3 4 5 6 7 8 9
+
+x = {"name":"Ayushi", "age":25} #dict
+print(type(x))
+
+x = {"apple","banana", "cherry"} #set
+print(type(x))
+
+x = ({"apple","banana", "cherry"}) #frozenset , immutable
+print(type(x))
+
+x = True #bool
+print(type(x)) #Output: <class 'bool'>
 
 
